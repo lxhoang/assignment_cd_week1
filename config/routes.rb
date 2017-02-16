f0c425849cf9707f912722718847604f7a0224ab
Hoang Le <xh.dhhv@gmail.com>
2017-02-16 23:50:01 +0700

    Create menu and contact us path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'menu' => 'home#index'
+  get 'contact_us' => "home#contact_us"
 
-  get 'home/contact_us'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
 end
