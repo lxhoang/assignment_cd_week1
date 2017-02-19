56dc89aaa28f0da89ec97fb30e5b5f075a49e914
Hoang Le <xh.dhhv@gmail.com>
2017-02-19 15:25:43 +0700

    edit change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root "home#index"
   
+
+  root "home#index"
+
+  get 'contact_us' => "home#contact_us"
+  get 'menu' => "home#menu"
+
   resources :food_items do
   	resources :orders
   end
   
-  get 'contact_us' => "home#contact_us"
-  get 'menu' => "home#menu"
-
  
 end
