4f4cc0879352c9368d921967897c68fc1e2a7783
Hoang Le <xh.dhhv@gmail.com>
2017-02-23 00:57:08 +0700

    sort and show food item details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,24 +7,20 @@ class HomeController < ApplicationController
 
   def menu
   	@sections = Section.all
-  	@food_items = FoodItem.all
-
+    @food_items = FoodItem.all
   	if params[:section_id].present?
   		@current_secion = Section.find(params[:section_id])
-  	end
-
-  	if params[:sort_column]
-  		@food_items = @food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
+      @food_items = @current_secion.food_items
   	end
 
   	if params[:search] && !params[:search].empty?
-  		# @sections = Article.search(params[:search]).order("created_at DESC")
     	@food_items = FoodItem.search(params[:search])
     	@search_keyword = params[:search]
   	end
 
-
+    if params[:sort_column]
+      @food_items = FoodItem.order("#{params[:sort_column]} #{params[:sort_direction]}")
+    end
   end
-
 
 end
